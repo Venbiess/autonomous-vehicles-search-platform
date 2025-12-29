954cc503d3bf3f7c2f1b63788de1822f27e8dd29
Venbiess <simpleonus5@mail.ru>
2025-12-29 20:09:02 +0000

    update docker

--- docker/models/run_docker.sh
+++ docker/models/run_docker.sh
@@ -1,7 +1,18 @@
+#!/usr/bin/env bash
+set -e
+
+START_JUPYTER=false
+
+if [[ "$1" == "--jupyter" ]]; then
+  START_JUPYTER=true
+fi
+
 docker rm -f avsp-models-dev-$USER 2>/dev/null || true
 
 docker run --rm -it \
+  -e START_JUPYTER=$START_JUPYTER \
   -p 8000:8000 \
+  -p 8888:8888 \
   -v "$(pwd)/../..":/app \
   -v "vscode-server-$USER":/root/.vscode-server \
   --name avsp-models-dev-$USER \
