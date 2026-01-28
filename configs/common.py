d3f01a12e99f6d146a289a5b751c6d99b0078e8e
Alekseev Andrey <simpleonus5@mail.ru>
2026-01-28 21:44:10 +0300

    add db

--- configs/common.py
+++ configs/common.py
@@ -1,3 +1,5 @@
+import os
+
 from .argoverse import *
 from .waymo import *
 from .nuscenes import *
@@ -13,5 +15,13 @@ S3_ENDPOINT_URL="http://minio:9000"
 S3_ACCESS_KEY_ID="minioadmin"
 S3_SECRET_ACCESS_KEY="minioadmin"
 
+# Postgres configuration
+POSTGRES_HOST = os.getenv("POSTGRES_HOST", "0.0.0.0")
+POSTGRES_PORT = int(os.getenv("POSTGRES_PORT", "7000"))
+POSTGRES_DB = os.getenv("POSTGRES_DB", "avsp")
+POSTGRES_USER = os.getenv("POSTGRES_USER", "postgres")
+POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD", "postgres")
+POSTGRES_SCHEMA = os.getenv("POSTGRES_SCHEMA", "public")
+POSTGRES_TABLE = os.getenv("POSTGRES_TABLE", "frames")
 
 
